cec74c8bfa56ea19042662c5b377231e39bce9f4
evertonagilar <evertonagilar@gmail.com>
2016-04-06 09:17:24 -0300

    curso

--- redes/CursoLinuxAvancado/conf_rede.sh
+++ redes/CursoLinuxAvancado/conf_rede.sh
@@ -6,8 +6,15 @@ meu_ip=192.168.0.3
 sudo ifconfig $iface down
 sudo ifconfig $iface $meu_ip netmask 255.255.255.0 up
 
-sudo route add -net 192.168.0.0 netmask 255.255.255.0 dev $iface
+#sudo route add -net 192.168.0.0 netmask 255.255.255.0 dev $iface
 sudo route add -net 192.168.1.0 netmask 255.255.255.0 dev $iface
-
 sudo route add -host 192.168.1.38 dev $iface reject
+
+sudo route add -net 164.41.130.70 netmask 255.255.255.0 dev $iface
+
+sudo route add default gw $meu_ip netmask 255.255.255.0 dev $iface
+
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward 
+
+sudo iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE
 
---
+++ redes/CursoLinuxAvancado/iniciasamba.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/usr/local/samba/sbin/smbd -D
+/usr/local/samba/sbin/winbindd -D
+/usr/local/samba/sbin/nmbd -D
+
