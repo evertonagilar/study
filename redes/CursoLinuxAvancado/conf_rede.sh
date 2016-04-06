020389fdc5882a3e3737e0ca6c98622d34733b66
evertonagilar <evertonagilar@gmail.com>
2016-04-06 10:58:37 -0300

    rede

--- redes/CursoLinuxAvancado/conf_rede.sh
+++ redes/CursoLinuxAvancado/conf_rede.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
+# Created by Everton Agilar
+# Date: 04/04/2016
 
-iface=enp2s0f5:1
+iface_rede_unb=enp2s0f5
+iface_rede_a=enp2s0f5:1
+
 meu_ip=192.168.0.3
+meu_ip_unb=164.41.130.70
+netmask=255.255.255.0
 
-sudo ifconfig $iface down
-sudo ifconfig $iface $meu_ip netmask 255.255.255.0 up
 
-#sudo route add -net 192.168.0.0 netmask 255.255.255.0 dev $iface
-sudo route add -net 192.168.1.0 netmask 255.255.255.0 dev $iface
-sudo route add -host 192.168.1.38 dev $iface reject
+start() {
 
-sudo route add -net 164.41.130.70 netmask 255.255.255.0 dev $iface
+	# levanta interfaces
+	sudo ifconfig $iface_rede_unb $meu_ip_unb netmask $netmask up
+	sudo ifconfig $iface_rede_a $meu_ip netmask $netmask up
 
-sudo route add default gw $meu_ip netmask 255.255.255.0 dev $iface
+	# adiciona as rotas
+	sudo route add -net 192.168.1.0 netmask $netmask dev $iface_rede_a
+	#sudo route add -net 164.41.130.70 netmask $netmask dev $iface_rede_unb
 
-sudo echo 1 > /proc/sys/net/ipv4/ip_forward 
+	# default gw
+	sudo route add default gw $meu_ip netmask $netmask dev $iface_rede_a
 
-sudo iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE
+	# habilita o roteamento
+	sudo echo 1 > /proc/sys/net/ipv4/ip_forward 
+	sudo iptables -t nat -A POSTROUTING -o $iface_rede_a -j MASQUERADE
 
+	# rotas para rejeitar
+	sudo route add -host 192.168.1.38 dev $iface_rede_a reject
+}
+
+stop() {
+	sudo ifconfig $iface_rede_a down
+	sudo ifconfig $iface_rede_unb down
+	
+	sudo route del -host 192.168.1.38 dev $iface_rede_a reject
+}
+
+case $1 in
+  start|stop) "$1" ;;
+esac
