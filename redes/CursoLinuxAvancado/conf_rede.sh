1720970c5cd4107f8fa390cceec4613a68dc4720
evertonagilar <evertonagilar@gmail.com>
2016-04-07 09:33:12 -0300

    rede

--- redes/CursoLinuxAvancado/conf_rede.sh
+++ redes/CursoLinuxAvancado/conf_rede.sh
@@ -17,41 +17,42 @@ start() {
 	# **** rede unb *****
 	
 	# levanta interfaces
-	sudo ifconfig $iface_rede_unb $meu_ip_unb netmask $netmask up
+	ifconfig $iface_rede_unb $meu_ip_unb netmask $netmask up
 
 	# adiciona as rotas
-	#sudo route add -host 164.41.101.4 dev $iface_rede_unb
+	#route add -host 164.41.101.4 dev $iface_rede_unb
 
 	# habilita o roteamento e nat
-	sudo echo 1 > /proc/sys/net/ipv4/ip_forward 
-	sudo iptables -t nat -A POSTROUTING -o $iface_rede_unb -j MASQUERADE
+	echo 1 > /proc/sys/net/ipv4/ip_forward 
+	iptables -t nat -A POSTROUTING -o $iface_rede_unb -j MASQUERADE
 
 
 	# **** rede a ****
 
-	sudo ifconfig $iface_rede_a $meu_ip netmask $netmask up
-	sudo route add -net 192.168.1.0 netmask $netmask dev $iface_rede_a
+	ifconfig $iface_rede_a $meu_ip netmask $netmask up
+	route add -net 192.168.1.0 netmask $netmask dev $iface_rede_a
 
 	# rotas para rejeitar
-	sudo route add -host 192.168.1.38 dev $iface_rede_a reject
+	route add -host 192.168.1.38 dev $iface_rede_a reject
 
 	
 	# cadastra os servidores dns
 	if ! grep $meu_dns /etc/resolv.conf  ; then
-		sudo echo nameserver $meu_dns >> /etc/resolv.conf ;
+		echo nameserver $meu_dns >> /etc/resolv.conf ;
 	fi
 
 	# default gw
-	sudo route add default gw $meu_gw netmask 0.0.0.0 dev $iface_rede_unb
+	route add default gw $meu_gw netmask 0.0.0.0 dev $iface_rede_unb
 
+	exit 0
 	
 }
 
 stop() {
-	sudo ifconfig $iface_rede_a down
-	sudo ifconfig $iface_rede_unb down
+	ifconfig $iface_rede_a down
+	ifconfig $iface_rede_unb down
 	
-	sudo route del -host 192.168.1.38 dev $iface_rede_a reject
+	route del -host 192.168.1.38 dev $iface_rede_a reject
 }
 
 case $1 in
