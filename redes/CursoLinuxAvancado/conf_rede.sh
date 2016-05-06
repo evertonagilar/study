ee406dee569d0737679005c32a07aefa4be9f450
evertonagilar <evertonagilar@gmail.com>
2016-05-06 19:54:05 -0300

    berco erlang

--- redes/CursoLinuxAvancado/conf_rede.sh
+++ redes/CursoLinuxAvancado/conf_rede.sh
@@ -2,56 +2,88 @@
 # Created by Everton Agilar
 # Date: 04/04/2016
 
-iface_rede_unb=enp2s0f5
-iface_rede_a=enp2s0f5:1
+# Interfaces para cada rede
+IFACE_REDE_UNB=enp2s0f5
+IFACE_REDE_A=enp2s0f5:1
+IFACE_REDE_B=enp2s0f5:2
+IFACE_REDE_C=enp2s0f5:3
 
-meu_ip=192.168.0.3
-meu_ip_unb=164.41.130.70
-netmask=255.255.255.0
-meu_dns=164.41.101.4
-meu_gw=164.41.130.1
+# O IPs de cada rede
+IP_REDE_A=192.168.10.1
+IP_REDE_B=192.168.15.1
+IP_REDE_C=192.168.20.1
+IP_REDE_UNB=164.41.130.70
+
+REDE_A=192.168.10.0
+REDE_B=192.168.15.0
+REDE_C=192.168.20.0
+
+
+
+NETMASK=255.255.255.0
+DEFAULT_GW=164.41.130.1
 
 
 start() {
 
-	# **** rede unb *****
+	# **** Rede unb *****
 	
 	# levanta interfaces
-	ifconfig $iface_rede_unb $meu_ip_unb netmask $netmask up
+	ifconfig $IFACE_REDE_UNB $IP_REDE_UNB netmask $NETMASK up
 
 	# adiciona as rotas
-	#route add -host 164.41.101.4 dev $iface_rede_unb
+	#route add -host 164.41.101.4 dev $IFACE_REDE_UNB
 
 	# habilita o roteamento e nat
 	echo 1 > /proc/sys/net/ipv4/ip_forward 
-	iptables -t nat -A POSTROUTING -o $iface_rede_unb -j MASQUERADE
+	iptables -t nat -A POSTROUTING -o $IFACE_REDE_UNB -j MASQUERADE
 
 
-	# **** rede a ****
+	# **** Rede A ****
 
-	ifconfig $iface_rede_a $meu_ip netmask $netmask up
-	route add -net 192.168.1.0 netmask $netmask dev $iface_rede_a
+	ifconfig $IFACE_REDE_A $IP_REDE_A netmask $NETMASK up
 
-	# rotas para rejeitar
-	route add -host 192.168.1.38 dev $iface_rede_a reject
+	route add -net 192.168.1.0 netmask $NETMASK dev $IFACE_REDE_A
 
-	
-	# cadastra os servidores dns
-	if ! grep $meu_dns /etc/resolv.conf  ; then
-		echo nameserver $meu_dns >> /etc/resolv.conf ;
-	fi
 
 	# default gw
-	route add default gw $meu_gw netmask 0.0.0.0 dev $iface_rede_unb
+	route add default gw $DEFAULT_GW netmask 0.0.0.0 dev $IFACE_REDE_UNB
+
+
+	# **** Rede B ****
+	
+	ifconfig $IFACE_REDE_B $IP_REDE_B netmask $NETMASK up
+
+	
+	# **** Rede C ****
+	
+	ifconfig $IFACE_REDE_C $IP_REDE_C netmask $NETMASK up
+
+
+	## Rotas
+	
+	#route add -net $REDE_A netmask 255.255.255.0 dev $IFACE_REDE_A
+	#route add -net $REDE_B netmask 255.255.255.0 dev $IFACE_REDE_B
+	#route add -net $REDE_C netmask 255.255.255.0 dev $IFACE_REDE_C
+
+
+	# Rotas rejeitar
+
+	#route add -host 192.168.1.38 dev $IFACE_REDE_A reject
+	
+		
+
 
 	exit 0
 }
 
 stop() {
-	route del -host 192.168.1.38 dev $iface_rede_a reject
+	route del -host 192.168.1.38 dev $IFACE_REDE_A reject
 
-	ifconfig $iface_rede_a down
-	ifconfig $iface_rede_unb down
+	ifconfig $IFACE_REDE_C down
+	ifconfig $IFACE_REDE_B down
+	ifconfig $IFACE_REDE_A down
+	ifconfig $IFACE_REDE_UNB down
 	
 	exit 0
 }
