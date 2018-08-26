8e4bef023fceca45ee947677efbd39fcf7577683
Everton de Vargas Agilar <evertonagilar@gmail.com>
2018-08-26 19:35:43 -0300

    c_interprester_passo2

--- c/c_interpreter/ci.c
+++ c/c_interpreter/ci.c
@@ -5,10 +5,52 @@
 #include <unistd.h>
 #include "utils.c"
 
-int token;              // current token
-char *src, *old_src;    // pointer to source code
-int poolsize;           // default size of text/data/stack
-int line;               // line number
+int token;                      // current token
+char *src, *old_src;            // pointer to source code
+int poolsize;                   // default size of text/data/stack
+int line;                       // line number
+int *text,                      // text segment. armazena código
+    *old_text,                  // for dump text segment
+    *stack;                     // stack
+char *data;                     // data segment. armazena variáveis
+
+// virtual machine registers
+int *pc,    // program counter, stores the next instruction to be run
+    *bp,    // base pointer, points to some elements on the stack. It is used in function call
+    *sp,    // stack pointer, points to the top of stack
+    ax,     // a general register that we used to store the result of an instruction
+    cycle;   
+
+// instructions
+enum {
+    LEA,     // make function calls
+    IMM,     // to put immediate <num> into register AX
+    JMP,     // unconditionally set the value PC register to <addr>
+    CALL,    // make function calls
+    JZ,      // jump if AX is zero
+    JNZ,     // jump if AX is not zero
+    ENT,     // make a new calling frame
+    ADJ,     // remove arguments from frame
+    LEV,     // restore call frame and PC
+    LI,      // load a int into AX from a memory address stored in AX
+    LC,      // load a char into AX from a memory address stored in AX
+    SI,      // store the int in AX into de memory address SP
+    SC,      // store the char in AX into de memory address SP
+    PUSH,    // push the value of AX into stack
+    OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD, // operators
+    OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT // intrinsic functions
+    }
+
+
+
+
+
+
+
+
+
+}
+
 
 // lexical analysis
 void next() {
