c692b413c95ca2567f5903eb24b4c1ecf2759407
Everton de Vargas Agilar <evertonagilar@gmail.com>
2018-09-16 19:02:45 -0300

    btree

--- c/c_interpreter/ci_btree.c
+++ c/c_interpreter/ci_btree.c
@@ -6,27 +6,53 @@
 #include <string.h>
 #include "ci_btree.h"
 
+// Private functions
+
+static bool ci_btree_insert2(ci_btree_t *btree, ci_btree_node_t **ppNode, const void *pData, size_t size);
+static const void *ci_btree_search2(const ci_btree_t *btree, const ci_btree_node_t *pNode, const void *key);
+
+
+// Helper functions
 
 void *ci_btree_default_key_func(const void *pData) {
     return pData;
 }
 
 
-ci_btree_t *ci_btree_new(ci_btree_cmp_func_t *cmpFunc, ci_btree_get_key_func_t *getKeyFunc) {
+// Implementation
+
+ci_btree_t *ci_btree_new(ci_btree_cmp_func_t *cmpFunc, ci_btree_compute_key_func_t *getKeyFunc) {
     if (cmpFunc == NULL){
         return NULL;
     }
     ci_btree_t *btree = malloc(sizeof(ci_btree_t));
     if (!btree){
         btree->pRoot = NULL;
-        btree->cmpFunc = cmpFunc;
-        btree->getKeyFunc = getKeyFunc == NULL ?  ci_btree_default_key_func : getKeyFunc;
+        btree->cmp = cmpFunc;
+        btree->compute_key = getKeyFunc == NULL ?  ci_btree_default_key_func : getKeyFunc;
     }
     return btree;
 }
 
 
-// Função auxiliar para inserir
+bool ci_btree_insert(ci_btree_t *btree, const void *pData, size_t size) {
+    if (btree == NULL || pData == NULL || size == 0) {
+        return false;
+    } else {
+        ci_btree_insert2(btree, &(btree->pRoot), pData, size);
+    }
+}
+
+const void *ci_btree_search(const ci_btree_t *btree, const void *key){
+    if (btree == NULL || key == NULL){
+        return NULL;
+    }
+    return ci_btree_search2(btree, btree->pRoot, key);
+}
+
+
+// private functions
+
 static bool ci_btree_insert2(ci_btree_t *btree, ci_btree_node_t **ppNode, const void *pData, size_t size) {
     ci_btree_node_t *pNode = *ppNode;
     if (pNode == NULL){
@@ -39,9 +65,9 @@ static bool ci_btree_insert2(ci_btree_t *btree, ci_btree_node_t **ppNode, const 
             return false;
         }
     }else{
-        const void *pKey1 = btree->getKeyFunc(pData),
-                   *pKey2 = btree->getKeyFunc(pNode->data);
-        if (btree->cmpFunc(pKey1, pKey2) < 0){
+        const void *pKey1 = btree->compute_key(pData),
+                *pKey2 = btree->compute_key(pNode->data);
+        if (btree->cmp(pKey1, pKey2) < 0){
             return ci_btree_insert2(btree, &pNode->left, pData, size);
         }else{
             return ci_btree_insert2(btree, &pNode->right, pData, size);
@@ -50,12 +76,18 @@ static bool ci_btree_insert2(ci_btree_t *btree, ci_btree_node_t **ppNode, const 
 }
 
 
-
-
-bool ci_btree_insert(ci_btree_t *btree, const void *pData, size_t size) {
-    if (btree == NULL || pData == NULL || size == 0) {
-        return false;
-    } else {
-        ci_btree_insert2(btree, &(btree->pRoot), pData, size);
+static const void *ci_btree_search2(const ci_btree_t *btree, const ci_btree_node_t *pNode, const void *key) {
+    if (pNode == NULL){
+        return NULL;
+    }
+    const void *key2 = btree->compute_key(pNode->data);
+    int cmp_result = btree->cmp(key, key2);
+    if (cmp_result == 0){
+        return pNode->data;
+    }else if (cmp_result < 0){
+        return ci_btree_search2(btree, pNode->left, key);
+    }else{
+        return ci_btree_search2(btree, pNode->right, key);
     }
 }
+
--- c/c_interpreter/ci_btree.h
+++ c/c_interpreter/ci_btree.h
@@ -21,7 +21,7 @@ typedef struct ci_btree_node  ci_btree_node_t;
 
 
 // Função para obter um key que corresponde aos dados
-typedef const void *ci_btree_get_key_func_t(const void *data);
+typedef const void *ci_btree_compute_key_func_t(const void *data);
 
 
 // Função para comparar keys
@@ -29,8 +29,8 @@ typedef int ci_btree_cmp_func_t(const void *pKey1, const void *pKey2);
 
 typedef struct {
     ci_btree_node_t *pRoot;
-    ci_btree_cmp_func_t *cmpFunc;
-    ci_btree_get_key_func_t  *getKeyFunc;
+    ci_btree_cmp_func_t *cmp;
+    ci_btree_compute_key_func_t  *compute_key;
 } ci_btree_t;
 
 
@@ -40,13 +40,18 @@ void *ci_btree_default_key_func(const void *pData);
 
 
 // Cria uma nova btree
-// cmpFunc      -> função para comparação
-// getKeyFunc   -> retorna a key para um data
-ci_btree_t *ci_btree_new(ci_btree_cmp_func_t *cmpFunc, ci_btree_get_key_func_t *getKeyFunc);
+// cmp      -> função para comparação
+// compute_key   -> retorna a key para um data
+ci_btree_t *ci_btree_new(ci_btree_cmp_func_t *cmpFunc, ci_btree_compute_key_func_t *getKeyFunc);
 
 
 // Adiciona um novo item
 bool ci_btree_insert(ci_btree_t *btree, const void *pData, size_t size);
+
+
+// Search item
+const void *ci_btree_search(const ci_btree_t *btree, const void *key);
+
 
 
 
