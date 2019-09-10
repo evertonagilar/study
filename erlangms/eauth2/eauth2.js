61bc59727ac0182bb82f0e707d240c210e228803
Everton de Vargas Agilar <evertonagilar@ufsm.br>
2019-09-10 15:02:39 -0300

    study

--- erlangms/eauth2/eauth2.js
+++ erlangms/eauth2/eauth2.js
@@ -5,16 +5,32 @@ const jErlangMs = e$ = function() {
             return this;
         },
 
-        autenticate({ server, client_id, redirect_uri, onSuccess, onAccessDenied, onError }) {
-            const url = `${server}?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
-            console.log('Autenticate url: ', url)
-            fetch(url)
-                //.then(response => response.json())
-                .then(response => {
-                    onSuccess(response)
-                });
+        autenticate({ server, client_id, client_secret, redirect_uri, onSuccess, onAccessDenied, onError }) {
+            const params = new URLSearchParams(location.search);
+            const code = params.get('code');
+            if (code == null || code == '') {
+                const url = `${server}?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
+                console.log(`Autenticate redirect to ${url}.`)
+                window.location.href = url;
+            } else {
+                console.log(`Autenticate code ${code} received from ${document.referrer}.`)
+                const url = `${server}?grant_type=authorization_code&code=${code}&client_id=${client_id}&redirect_uri=${redirect_uri}`;
+                const authorizatoinHeader = 'Basic ' + btoa(client_id) + ':' + btoa(client_secret);
+                const optionsFetch = {
+                    method: 'POST',
+                    //mode: 'cors',
+                    headers: new Headers({
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'authorizaton': authorizatoinHeader
+                    })
+                }
+                fetch(url, optionsFetch)
+                    //.then(response => response.json())
+                    .then(response => {
+                        onSuccess(response)
+                    });
+            }
             return this;
         }
-
     }
 };
