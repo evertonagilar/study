1fbcbcedb0c5a3acad9b0e3e84c1b8e5696f393c
Everton de Vargas Agilar <evertonagilar@gmail.com>
2020-02-13 20:12:23 -0300

    estudo

--- nasm/printargs.asm
+++ nasm/printargs.asm
@@ -33,7 +33,7 @@ section .code
 
 
 _start:
-   
+
     ; imprime o título do programa
     sub rsp, 16                             ; aloca espaço na pilha
     mov qword [rsp+8], titulo_str_len       ; primeiro a empilhar (size)
@@ -44,10 +44,11 @@ _start:
     ; salva o número de argumentos (que está na pilha) na variável argc
     pop rcx
     dec rcx
+    add rsp, 8                              ; remove o nome do programa da pilha
     mov [argc], rcx
 
     ; verifica número de argumentos
-    cmp rcx, 0
+    cmp rcx, 1
     jz sem_argumentos
 
 
@@ -55,7 +56,7 @@ _start:
 print_arg:
     pop rax
     sub rsp, 16                             ; aloca espaço na pilha
-    mov qword [rsp+8], 15                    ; primeiro a empilhar (size)
+    mov qword [rsp+8], 15                   ; primeiro a empilhar (size)
     mov qword [rsp], rax                    ; segundo empilhar (msg)
     call println
     add rsp, 16                             ; limpa a pilha
@@ -92,11 +93,18 @@ exit:
 println:
     mov r10, rsp               ; salva a pilha 
 
+    ; calcula o tamanho da string
+    mov rax, [rsp+8]           ;titulo_str         
+    sub rsp, 8
+    mov [rsp], rax
+    call strlen
+    mov rdx, rax
+    add rsp, 8
+
     ; print msg
     mov rax, SYS_WRITE
     mov rdi, STD_OUT
     mov rsi, [rsp+8]           ;titulo_str         
-    mov rdx, [rsp+16]          ;titulo_str_len     
     syscall
 
     ; print LF
@@ -110,4 +118,19 @@ println:
     mov rsp, r10               ; restaura a pilha
     ret
 
+
+; retorna tamanho da string
+; convenção de chamada: cdecl
+; void strlen(char *msg)
+strlen:
+    mov rdi, [rsp+8]            ; coloca o endereço da string em rdi e rsi
+    mov rax, rdi
+nextchar:    
+    cmp byte [rax], 0h
+    jz strlen_fim
+    inc rax
+    jmp nextchar
+strlen_fim:
+    sub rax, rdi
+    ret
 
