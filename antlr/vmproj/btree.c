1cb963d782efb22de5b46022e0bcb998b2c7b01f
Everton de Vargas Agilar <evertonagilar@gmail.com>
2022-06-14 21:43:51 -0300

    vmproj

--- antlr/vmproj/btree.c
+++ antlr/vmproj/btree.c
@@ -6,16 +6,31 @@
 #include <malloc.h>
 #include "btree.h"
 
+
+/*
+ *
+ * Ua árvore B de ordem m é uma árvore que satisfaz as seguintes propriedades:
+ *        Cada nó tem no máximo m filhos.
+ *        Cada nó interno, exceto a raiz, tem pelo menos m/2 filhos.
+ *        Cada nó não folha tem pelo menos dois filhos.
+ *        Todas as folhas aparecem no mesmo nível e não carregam nenhuma informação.
+ *        Um nó não folha com k filhos contém k −1 chaves.
+ *
+ *
+ */
+
+
 // ********************************** private functions **********************************
 
 /*
  * Cria um novo node com m childrens
  *
  */
-btree_node_t *create_btree_node(int m) {
-    btree_node_t * node = malloc(sizeof (btree_node_t));
-    node->m = m;
-    return node;
+btree_page_t *btree_page_new(int m) {
+    btree_page_t * new_page = malloc(sizeof (btree_page_t));
+    new_page->m = m;
+    new_page->is_leaf = true;
+    return new_page;
 }
 
 /*
@@ -23,50 +38,100 @@ btree_node_t *create_btree_node(int m) {
  * Cria a btree
  *
  */
-btree_t *create_btree(){
+btree_t *btree_new(){
     btree_t * btree = malloc(sizeof (btree_t));
-    btree->root = create_btree_node(0);
+    btree->root = btree_page_new(0);
     btree->n = 0;
     btree->height = 0;
     return btree;
 }
 
+
+btree_key_entry *btree_key_entry_new(int key){
+    btree_key_entry *entry = malloc(sizeof(btree_key_entry));
+    entry->key = key;
+    return entry;
+}
+
 /*
  *
  *
+ */
+btree_page_split_t *btree_page_split(btree_page_t *page){
+    btree_page_split_t *result = malloc(sizeof(btree_page_split_t));
+    result->left = btree_page_new(page->m/2-1);  // subtrai 1 porque a maior key do lado esquerdo é promovida
+    result->right = btree_page_new(page->m/2);
+    result->key_do_meio = page->keys[page->m/2-1];
+
+    // Copia as keys menores que key_do_meio para left
+    for (int i = 0; i < page->m/2-1; i++){
+        result->left->keys[i] = page->keys[i];
+    }
+    // Copia as keys maiores que key_do_meio para right
+    btree_key_entry **pentry = result->right->keys;
+    for (int i = page->m/2; i < page->m; i++){
+        *pentry++ = page->keys[i];
+    }
+
+    return result;
+}
+
+/*
  *
  */
-btree_node_t *add_btree_node(btree_node_t *h, int key, int height){
+btree_page_t *btree_page_insert(btree_page_t *page, int key, int height){
     int i;
-    for (i = 0; i < h->m; i++){
-        if (key < h->children[i].key) break;
+
+    // Se a página é folha, vamos descobrir onde ela vai ser amazenada no array keys
+    if (page->is_leaf){
+
+        // Se a página já atingiu o limite, vamos precisar dividir em duas páginas (operação split)
+        if (page->m == ORDER){
+            btree_page_split_t *split = btree_page_split(page);
+            page->is_leaf = false;      // não é mais folha
+            page->
+
+        }else {
+            // Encontra a posição nas keys onde a nova key deve ficar
+            for (i = 0; i < page->m; i++) {
+                if (key < page->keys[i]->key) break;
+            }
+
+            // Agora, move todas as chaves uma posição para frente para abrir espaço para a nova key
+            for (int j = page->m; j > i; j--) {
+                page->keys[j] = page->keys[j - 1];
+            }
+
+            // Armazenamos a nova entrada da key nesta posição
+            page->keys[i] = btree_key_entry_new(key);
+
+            // e incrementamos a quantidade de keys que agora há nesta página
+            page->m++;
+        }
     }
-    for (int j = h->m; j > i; j--){
-        h->children[j] = h->children[j-1];
-    }
-    h->children[i].key = key;
-    h->m++;
-    return h;
+    return page;
 }
 
 
 // ********************************** public functions **********************************
 
-bool *add_btree(btree_t *btree, int key){
-    btree_node_t *node = add_btree_node(btree->root, key, btree->height);
+bool *btree_insert(btree_t *btree, int key){
+    btree_page_t *node = btree_page_insert(btree->root, key, btree->height);
     return node != NULL;
 }
 
 int main(int argc, char **argv) {
     puts("Programa teste btree\n");
 
-    btree_t *btree = create_btree();
+    btree_t *btree = btree_new();
     bool inseriu;
-    inseriu = add_btree(btree, 10);
-    inseriu = add_btree(btree, 5);
-    inseriu = add_btree(btree, 15);
-    inseriu = add_btree(btree, 12);
-    inseriu = add_btree(btree, 7);
+
+    // 5  10  12  15
+    inseriu = btree_insert(btree, 10);
+    inseriu = btree_insert(btree, 5);
+    inseriu = btree_insert(btree, 15);
+    inseriu = btree_insert(btree, 12);
+    inseriu = btree_insert(btree, 7);
 
     printf("Btree criado com %d elementos.", btree->height);
 
--- antlr/vmproj/btree.h
+++ antlr/vmproj/btree.h
@@ -7,27 +7,35 @@
 
 #include <stdbool.h>
 
-#define MAX_CHILDREN_NODE 4
+#define ORDER 4
 
 typedef struct {
     int key;
-    struct btree_node_t *next;
-} btree_node_entry;
+    void *value;
+} btree_key_entry;
 
-typedef struct btree_node_t{
-    int m;                                           // número de filhos
-    btree_node_entry children[MAX_CHILDREN_NODE];    // array de childrens
-} btree_node_t;
+
+typedef struct {
+    int m;                                       // número de filhos da página
+    bool is_leaf;                                // indica se a página é uma folha
+    btree_key_entry *keys[ORDER];                // array de keys entries
+    struct btree_page_t *children[ORDER-1];
+} btree_page_t;
 
 typedef struct {
     int n;                              // quantos elementos estão na btree
     int height;                         // altura da btree
-    btree_node_t *root;                 // root da btree
+    btree_page_t *root;                 // root da btree
 } btree_t;
 
+typedef struct {
+    btree_page_t *left;
+    btree_page_t *right;
+    btree_key_entry *key_do_meio;
+} btree_page_split_t;
 
-btree_t *create_btree();
-bool *add_btree(btree_t *btree, int key);
+btree_t *btree_new();
+bool *btree_insert(btree_t *btree, int key);
 
 
 #endif //VMPROJ_BTREE_H
